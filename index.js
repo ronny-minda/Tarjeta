080faa5234fa4b78235881dea953df5508b82164
ronny-minda <ronny.michael.minda.vera@gmail.com>
2021-08-17 11:19:08 -0500

    todo listo

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+const tarjeta = document.querySelector(".tarjeta");
+
+
+
+function mouseDown() {
+    console.log('mouseDown')
+    document.getElementById("pantalla").onmousemove = function(event) {myFunction(event)};
+    function myFunction(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        console.log("Coordinates: (" + x + "," + y + ")")
+        tarjeta.style.left = x-250 + "px";
+        tarjeta.style.top = y-280 + "px";
+    }
+}
+
+function mouseUp() {
+    console.log('mouseUp')
+    // document.getElementById("pantalla").onmousemove = function(event) {myFunction(event)};
+    document.getElementById("pantalla").onmousemove = function(event) {myFunction(event)};
+    function myFunction(e) {
+
+    }
+}
+
